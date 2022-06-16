2be51126b106df623faa879f3d1031fdff1cd947
Bob Nearents <bobnearents@gmail.com>
2022-06-16 16:41:29 -0700

    make the dang thing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Song from "./Song/Song";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLogInPage, setIsLogInPage] = useState(false);
+  const [currentSong, setCurrentSong] = useState();
+  const [currentLyrics, setCurrentLyrics] = useState();
+  const [authUrl, setAuthUrl] = useState();
+
+  useEffect(() => {
+    if (!currentSong && !isLogInPage) {
+      setIsLoading(true);
+    } else {
+      setIsLoading(false);
+    }
+  }, [currentSong, isLogInPage]);
+
+  useEffect(() => {
+    const getAuthUrl = async () => {
+      const response = await fetch("http://localhost:5000/login", {
+        method: "GET",
+        mode: "cors",
+      });
+
+      const result = await response.text();
+      setAuthUrl(result);
+    };
+    const getToken = async () => {
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = searchParams.get("code");
+      const response = await fetch(
+        `http://localhost:5000/access-token?code=${code}`,
+        {
+          method: "GET",
+          mode: "cors",
+        }
+      );
+      const result = await response.json();
+      localStorage.setItem("access_token", result.access_token);
+      localStorage.setItem("refresh_token", result.refresh_token);
+    };
+    const getCurrentSong = async () => {
+      const songResponse = await fetch(
+        `http://localhost:5000/current-song?token=${localStorage.getItem(
+          "access_token"
+        )}`,
+        {
+          method: "GET",
+          mode: "cors",
+        }
+      );
+
+      const songResult = await songResponse.json();
+      const artist = songResult.item.artists[0].name;
+      const title = songResult.item.name;
+      const lyricsResponse = await fetch(
+        `http://localhost:5000/lyrics?artist=${artist}&title=${title}`,
+        {
+          method: "GET",
+          mode: "cors",
+        }
+      );
+      const lyricsResult = await lyricsResponse.text();
+      setCurrentLyrics(JSON.parse(lyricsResult).lyrics);
+      setCurrentSong(songResult);
+    };
+    //if we already have an access token then we can just get song info
+    if (localStorage.access_token) {
+      getCurrentSong();
+    } else if (localStorage.refresh_token) {
+      console.log("time to refresh token???", localStorage);
+    }
+    //otherwise if user has already authorized, then lets get the tokens
+    else if (window.location.search.length) {
+      getToken();
+    } else {
+      //otherwise get user to authorize
+      getAuthUrl();
+      setIsLogInPage(true);
+    }
+  }, []);
+
+  return isLoading ? (
+    <Loading />
+  ) : isLogInPage ? (
+    <LogIn authUrl={authUrl} />
+  ) : (
+    <>
+      <Song song={currentSong} />
+      <p className="current-lyrics">{currentLyrics}</p>
+    </>
   );
+}
+
+function Loading() {
+  return <div>loading...</div>;
+}
+
+function LogIn(props) {
+  return <a href={props.authUrl}>Log in to Spotify to continue...</a>;
 }
 
 export default App;
---
+++ src/Song/Song.js
@@ -0,0 +1,41 @@
+export default function Song(props) {
+  if (!props.song) return;
+  const { item: song } = props.song;
+  const { name, album, artists } = song;
+  const track = { name, link: song.external_urls.spotify };
+  const albumObj = {
+    name: album.name,
+    link: album.external_urls.spotify,
+    img: album.images[1].url,
+  };
+
+  return (
+    <div className="current-song">
+      <Album album={albumObj} />
+      <div className="text-container">
+        <a className="song-name" href={track.link}>
+          {track.name}
+        </a>
+        <Artists artists={artists} />
+      </div>
+    </div>
+  );
+}
+
+const Album = (props) => {
+  return (
+    <a className="album-container" href={props.album.link}>
+      <img src={props.album.img} alt={props.album.name} />
+    </a>
+  );
+};
+
+const Artists = (props) => {
+  return (
+    <div className="artists-container">
+      {props.artists.map((artist) => {
+        return <a href={artist.external_urls.spotify}>{artist.name}</a>;
+      })}
+    </div>
+  );
+};
